5895941bd5e363308f0b7bb778cc577f91b8ebfa
Gopika <babugopika13@gmail.com>
2021-06-18 23:27:37 -0400

    commit3

---
+++ flowers-app/src/components/Order/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import OrderForm from './OrderForm'
+import { useForm } from '../../hooks/useForm';
+
+const generateOrderNumber = () =>Math.floor(100 + Math.random() * 900).toString();
+
+const getFreshModelObject =() =>({
+    orderMasterId :0,
+    orderNumber : generateOrderNumber(),
+    customerId : 0,
+    pMethod : 'none',
+    gTotal : 0,
+    deletedOrderItemIds:'',
+    orderDetails:[]
+})
+
+export default function Order() {
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetFormControls
+    } = useForm(getFreshModelObject);
+
+    return (
+       <OrderForm
+           { ...{ values, errors, handleInputChange }}
+           
+       />
+    )
+}
